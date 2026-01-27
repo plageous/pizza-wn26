a7a7881cb432d8236e2b58aa1d710d82cf7f13ea
KJ <122484032+plageous@users.noreply.github.com>
2026-01-27 14:05:48 -0800

    Added errors for blank sections. Not finished.

--- pizza.js
+++ pizza.js
@@ -1,1 +1,24 @@
-alert("PIZZA")
+// alert on pizza-form submission
+document.getElementById("pizza-form").onsubmit = () => {
+    let isValid = true;
+
+    let fname = document.getElementById("fname").value.trim();
+    let lname = document.getElementById("lname").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+
+    if (!fname) {
+        document.getElementById("err-fname").style.display = "block";
+        isValid = false;
+    }
+    if (!lname) {
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+    if (!email) {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+}

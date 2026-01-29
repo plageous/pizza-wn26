75a45d69908905c034de9252e80c0fdd5bd8b431
KJ <122484032+plageous@users.noreply.github.com>
2026-01-29 13:52:25 -0800

    Added nodemon (not implemeneted yet) and fixed JS errors.

---
+++ app.js
@@ -0,0 +1,23 @@
+// express import statement
+import express from 'express';
+
+// instance of app object
+const app = express();
+
+// server port
+const PORT = 3000;
+
+// static file serving
+// tells express to use files in "public" directory
+app.use(express.static('public'));
+
+// main route ('/')
+app.get('/', (req, res) => {
+    // sends home.html file to client
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// start server, listen on PORT
+app.listen(PORT, () => {
+    console.log(`Server running at https://localhost:${PORT}`);
+});
